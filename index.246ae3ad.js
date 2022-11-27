b158abd0054bd6b1a0608e242ca4c60d3506955f
al99vin <50546195+al99vin@users.noreply.github.com>
2022-11-27 19:00:10 +0000

    Deploying to gh-pages from @ al99vin/goit-js-hw-11@007d502c3a9a5c1b69d91040ab2580c27b8e99c7 🚀

---
+++ index.246ae3ad.js
@@ -0,0 +1,2 @@
+refs={form:document.querySelector("#search-form"),loadMoreBtn:document.querySelector(".load-more")};
+//# sourceMappingURL=index.246ae3ad.js.map
---
+++ index.7a19ff44.js
@@ -0,0 +1,2 @@
+refs={form:document.querySelector("#search-form"),loadMoreBtn:document.querySelector(".load-more")};
+//# sourceMappingURL=index.7a19ff44.js.map
